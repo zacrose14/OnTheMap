a1dd1fbef535416f6defd90942a14f5e69e782a4
Zac Rose <zac@insightmobiledevelopment.com>
2017-12-01 13:12:12 -0500

    Added remaining scenes to storyboard and linked to correct VCs. Added AutoLayout to List and Maps. Created corresponding segues to link project together. More work on Data Structure. Added Correct Nav Bars to map and list scenes.

--- OnTheMap/Model/Clients/Parse/ParseConstants.swift
+++ OnTheMap/Model/Clients/Parse/ParseConstants.swift
@@ -14,6 +14,7 @@ extension ParseClient {
     struct Constants {
         
         static let ApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let AppID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
         
         static let ApiScheme = "https"
         static let ApiHost = "parse.udacity.com"
