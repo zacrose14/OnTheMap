f2263cec0a79a34000f1a51a6dad238ef550a8c4
Zac Rose <zac@insightmobiledevelopment.com>
2017-12-01 17:29:08 -0500

    Finished adding some more UI. Added last VC to handle Adding Student Location Map. Removed unneeded boiler plate code. Will add auto layout to all screens tomorrow. Ready to move back into work with data models for API.

---
+++ OnTheMap/Controller/AddStudentLocationMapVC.swift
@@ -0,0 +1,23 @@
+//
+//  AddStudentLocationMapVC.swift
+//  OnTheMap
+//
+//  Created by Zachary Rose on 12/1/17.
+//  Copyright © 2017 Zachary Rose. All rights reserved.
+//
+
+import UIKit
+
+class AddStudentLocationMapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
